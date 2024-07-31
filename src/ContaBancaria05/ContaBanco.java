18a0ee17497d3748a2c551a9bce48b9b31ce430b
Moizés Jr <moizes.tj@gmail.com>
2024-07-31 06:18:49 -0300

    ContaBanco e metodos

--- src/ContaBancaria05/ContaBanco.java
+++ src/ContaBancaria05/ContaBanco.java
@@ -9,9 +9,63 @@ public class ContaBanco {
   public boolean status;
 
   public ContaBanco() {
+    this.setSaldo(0);
+    this.setStatus(false);
+  }
 
-    status = false;
-    saldo = 0;
+  public void abrirConta(String tipo) {
+    this.setTipo(tipo);
+    this.setStatus(true);
+    if (tipo.equals("cp")) {
+      this.setSaldo(150);
+      System.out.println("Conta poupança aberta com sucesso");
+    } else if (tipo.equals("cc")) {
+      this.setSaldo(50);
+      System.out.println("Conta corrente aberta com sucesso");
+    } else {
+      System.out.println("erro! Escolha uma conta CC ou CP");
+      this.setStatus(false);
+    }
+  }
+
+  public void fecharConta() {
+    if (this.getSaldo() == 0) {
+      this.setStatus(false);
+      System.out.println("Conta fechada com sucesso");
+    } else {
+      System.out.println("Impossivel fechar a conta com saldo ou debito");
+    }
+  }
+
+  public void depositar(Float saldo) {
+    if (this.getStatus()) {
+      this.setSaldo(getSaldo() + saldo);
+      System.out.println("Deposito feito com sucesso!");
+    } else {
+      System.out.println("Você não tem conta para fazer deposito");
+    }
+  }
+
+  public void sacar(Float saldo) {
+    if (this.getStatus()) {
+      if (this.getSaldo() > saldo) {
+        this.setSaldo(getSaldo() - saldo);
+        System.out.println("Saque realizado com sucesso");
+      } else {
+        System.out.println("Saldo insuficiente");
+      }
+    } else {
+      System.out.println("Conta inexistente para saque!");
+    }
+  }
+
+  public void pagarMensalidade() {
+    if (this.getStatus() || this.getTipo() == "cc") {
+      this.setSaldo(getSaldo() - 12);
+    } else if (status == true || tipo == "cp") {
+      this.setSaldo(getSaldo() - 20);
+    }
+    System.out.println("Mesanlidade paga com sucesso!");
   }
 
   public int getNumConta() {
@@ -46,12 +100,21 @@ public class ContaBanco {
     this.saldo = saldo;
   }
 
-  public boolean isStatus() {
+  public boolean getStatus() {
     return status;
   }
 
   public void setStatus(boolean status) {
     this.status = status;
+  }
+
+  @Override
+  public String toString() {
+    return "\nContaBanco [numConta=" + numConta +
+        ", \ntipo=" + tipo +
+        ", \ndono=" + dono +
+        ", \nsaldo=" + saldo +
+        ", \nstatus=" + status + "]";
   }
 
 }
